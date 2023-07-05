83a3396ae8d48a3555bf502a2aebd1251878dae0
Colin Alway <colin.alway@gmail.com>
2023-07-05 15:22:02 +0100

    cluster event listener, python

---
+++ Content/Python/cluster_event.py
@@ -0,0 +1,58 @@
+#!python
+"""
+convert int to/from bytes array:
+>>> num = 42
+>>> big = num.to_bytes(4)
+>>> little = num.to_bytes(4, byteorder="little")  # default byteorder is "big"
+>>> [x for x in big]
+[0, 0, 0, 62]
+>>> [x for x in little]
+[62, 0, 0, 0]
+"""
+import json
+import socket
+import sys
+import time
+
+HOST, PORT, PORT_JSON = "localhost", 41004, 41003
+PORT = PORT_JSON
+
+def main():
+    print("emit cluster event")
+
+    event = {
+        "Name": "quit",
+        "Type": "command",
+        "Category": "",
+        "Parameters":{}
+    }
+    event_string = json.dumps(event)
+    event_string = '{"Name":"quit","Type":"command","Category":"","Parameters":{}}'
+    print("event_string '{}'".format(event_string))
+    event_length = len(event_string)
+    print("length {} {}".format(event_length, event_length.to_bytes(4)))
+
+
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(event_length.to_bytes(4))
+        sock.sendall(str.encode(event_string))
+
+        # Receive data from the server and shut down
+        # received = sock.recv(1024)
+
+        print("done")
+    except ConnectionRefusedError:
+        print("refused")
+    finally:
+        sock.close()
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(1)
